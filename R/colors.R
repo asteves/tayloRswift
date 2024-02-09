9912c429dfd17ff7bbd8081f71cff52d2102f172
Victoria Silverman <71465807+victoriasilverman@users.noreply.github.com>
2024-02-09 12:00:03 -0500

    Update colors.R
    added 1989 cover variants and fixed fearless

--- R/colors.R
+++ R/colors.R
@@ -3,8 +3,8 @@
 #' @export
 swift_palettes <- list(
   `taylorSwift` = c("#61b6cc", "#577f3f" ,"#e3e9f3", "#0a1605","#fddac7","#81a757"),
-  `fearless` = c("#b68f51","#5b3617","#f7eabe", "#ecd59f","#825c2d"),
-  `fearlessTV` = c("#5C3C24", "#E3D1A3", "#BC9C64", "#FDFAF6", "#997A4B", "#AC9167")
+  `fearless` = c("#DCBE83", "#88642C", "#EDE7D9", "#EFD998", "#49260A", "#EFB391"),
+  `fearlessTV` = c("#b68f51", "#5b3617", "#f7eabe", "#ecd59f", "#825c2d")
   `speakNow` = c("#4b2671","#5e291c", "#f3d8c4","#f3bf73","#ffffff"),
   `speakNowTV` = c("#351E2E","#481E52","#AE6D4F","#884885","#C44D56","#A0659F")
   `speakNowLive` = c("#fce178", "#969696", "#871d20", "#090708", "#fafaf9"),
@@ -12,7 +12,9 @@ swift_palettes <- list(
   `taylorRed` = c("#BFBCAA", "#A6836F", "#73564C", "#731803", "#400303"),
   `taylor1989` = c("#b1532a","#84697f","#cbb593","#a88f92","#e8eadf", "#43475b"),
   `TS1989` = c("#D9523F", "#D1CFC2", "#ACBCCC","#588383","#AA6F4E","#8898A1" )
-  `TS1989B` = c("#2B3849","#6C1414","#C8AA86","#D0C9B9","#888D50","#313C40")
+  `SunriseBoulevard1989` = "#2B3849","#6C1414","#C8AA86","#D0C9B9","#888D50","#313C40"
+  `AquamarineGreen1989` = "#101C18", "#C19298", "#5B96A5","#90A5A8","#9A241F","#938C7C"
+  `RoseGardenPink1989` = "#8A9DAC", "#9E948A","#DD8253","#B4AEAF","#B6723C","7F8491"
   `reputation` = c("#060606","#6e6e6e","#fefefe","#cacaca", "#060606","#8c8c8c"),
   `lover` = c("#b8396b","#ffd1d7","#fff5cc","#76bae0","#b28f81","#54483e"),
   `folklore` = c("#272727","#5c5c5c","#bababa", "#f8f8f8"),
@@ -21,7 +23,7 @@ swift_palettes <- list(
   `midnightsBloodMoon` = c("#651D32", "#80433F", "#BC7B55", "#AD9291", "#E9E8E4"),
   `midnightsJadeGreen` = c("#E8E7E3", "#CE986B", "#2F3C30", "#0B0B08", "#B98645", "#8D8C7A"),
   `midnightsMahogany` = c("#AE9975", "#2B181F", "#E2E1DD", "#948266", "#AD8E49")
-  `TSTTPS` = c("#8D8171", "#C6C0B8", "#49433B", "#FDFDFD", "#322B25","#ADA69C")
+  `TSTTPD` = c("#8D8171", "#C6C0B8", "#49433B", "#FDFDFD", "#322B25","#ADA69C")
 )
 
 
